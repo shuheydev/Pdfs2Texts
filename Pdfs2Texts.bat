3c72cdd7604af7a2efb172068bb756c7bbe3f70d
shuhey <shuhey.dev@gmail.com>
2018-10-29 13:09:35 +0900

    実行時にファイルパスの入力をもとめるように変更。

--- Pdfs2Texts.bat
+++ Pdfs2Texts.bat
@@ -4,6 +4,10 @@ rem 処理対象のPDFファイルのパスが記述されたテキストファイルを読み込み、
 rem PDFファイルを一つずつ処理する。
 rem %1は1つ目の引数。
 
-for /f %%a in (%1) do (
+set /P listFilePath="処理対象ファイルのパスが記述されたテキストファイルを指定してください（絶対パス）："
+
+for /f %%a in (%listFilePath%) do (
     pdftotext -layout %%a
-) 
+)
+
+pause
